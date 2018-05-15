d6f635c9e5130d0aeda00b09d8399e0dd2f19a70
kntmrkm <kntmrkm@gmail.com>
2018-05-15 20:49:02 +0900

    LineLogin.java Android

--- src/android/LineLogin.java
+++ src/android/LineLogin.java
@@ -4,8 +4,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
+import com.linecorp.linesdk.LineApiResponse;
 import com.linecorp.linesdk.LineApiResponseCode;
 import com.linecorp.linesdk.LineProfile;
+import com.linecorp.linesdk.api.LineApiClient;
+import com.linecorp.linesdk.api.LineApiClientBuilder;
 import com.linecorp.linesdk.auth.LineLoginApi;
 import com.linecorp.linesdk.auth.LineLoginResult;
 
@@ -18,6 +21,7 @@ public class LineLogin extends CordovaPlugin {
 
     String channelId;
     CallbackContext callbackContext;
+    private static LineApiClient lineApiClient;
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
@@ -25,12 +29,45 @@ public class LineLogin extends CordovaPlugin {
         if (action.equals("initialize")) {
             JSONObject params = data.getJSONObject(0);
             channelId = params.get("channel_id").toString();
+
+            LineApiClientBuilder apiClientBuilder = new LineApiClientBuilder(this.cordova.getActivity().getApplicationContext(), channelId);
+            lineApiClient = apiClientBuilder.build();
+
             return true;
         } else if (action.equals("login")) {
             Context context = this.cordova.getActivity().getApplicationContext();
             Intent loginIntent = LineLoginApi.getLoginIntent(context, channelId);
             this.cordova.startActivityForResult((CordovaPlugin) this, loginIntent, 0);
             this.callbackContext = callbackContext;
+            return true;
+        } else if (action.equals("logout")) {
+            try {
+                lineApiClient.logout();
+                callbackContext.success();
+            } catch (Exception e) {
+                callbackContext.error(-1);
+            }
+            return true;
+        } else if (action.equals("getAccessToken")) {
+            String accessToken = lineApiClient.getCurrentAccessToken().getResponseData().getAccessToken();
+            callbackContext.success(accessToken);
+            return true;
+        } else if (action.equals("verifyAccessToken")) {
+            LineApiResponse verifyResponse = lineApiClient.verifyToken();
+            if (verifyResponse.isSuccess()) {
+                callbackContext.success();
+            } else {
+                callbackContext.error(-1);
+            }
+            return true;
+        } else if (action.equals("refreshAccessToken")) {
+            try {
+                lineApiClient.refreshAccessToken();
+                String accessToken = lineApiClient.getCurrentAccessToken().getResponseData().getAccessToken();
+                callbackContext.success(accessToken);
+            } catch (Exception e) {
+                callbackContext.error(-1);
+            }
             return true;
         } else {
             return false;
@@ -47,9 +84,7 @@ public class LineLogin extends CordovaPlugin {
             try {
                 json.put("userID", profile.getUserId());
                 json.put("displayName", profile.getDisplayName());
-                json.put("accessToken", profile.getAccessToken());
                 json.put("pictureURL", profile.getPictureUrl());
-                json.put("statusMessage", profile.getStatusMessage());
                 callbackContext.success(json);
             } catch (JSONException e) {
                 callbackContext.error(-1);
